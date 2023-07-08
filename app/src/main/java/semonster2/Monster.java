b1a106cfbd6a0c03f7395757605fc03f2a14c68a
Masaru Horiuchi <Horiuchi@github.com>
2023-07-08 17:37:05 +0900

    add Monster.java

---
+++ app/src/main/java/semonster2/Monster.java
@@ -0,0 +1,30 @@
+package semonster2;
+//import java.util.Random;
+
+public class Monster {
+  String name;
+  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
+
+  Monster(String name,int rare) {
+    this.rare=rare;
+    if(rare>=3){
+      this.name=this.evolutionMonster(name);
+    }else{
+      this.name=name;
+    }
+  }
+
+  String summonMonster(int mnumber) {
+    String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+    return monsters[mnumber];
+  }
+
+  String evolutionMonster(String mname){
+    return "キング"+mname;
+  }
+
+  @Override
+  public String toString() {
+    return this.name + ":レア度[" + this.rare + "]";
+  }
+}
---
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -0,0 +1,27 @@
+package semonster2;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MonsterTest {
+    @Test public void monsterHaveMonsterNames(){
+        Monster classUnderTest=new Monster("スライム",1);
+        assertNotNull("monsters should have names",classUnderTest.toString());
+    }
+    @Test public void testMonsterReturnNameAndRarity(){
+        Monster classUnderTest=new Monster("スライム",2);
+        assertEquals(classUnderTest.name+":レア度["+classUnderTest.rare+"]",classUnderTest.toString());
+    }
+    @Test public void summonMonsterTest(){
+        Monster classUnderTest=new Monster("スライム",4);
+        String expected[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+        for(int i=0;i<5;i++){
+            assertEquals(expected[i],classUnderTest.summonMonster(i));
+        }
+    }
+    @Test public void evolutionMonsterTest(){
+        Monster classUnderTest = new Monster("スライム",0);
+        String expected="キングスライム";
+        assertEquals(expected,classUnderTest.evolutionMonster("スライム"));
+    }
+}
